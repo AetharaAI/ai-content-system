dd0dc2677fc7ac534574dc00c11ad4b831c8b751
CJ <cgibson@aetharaai.org>
2025-06-03 06:37:33 -0400

    pydantic

--- services/shared/config.py
+++ services/shared/config.py
@@ -1,6 +1,6 @@
 import os
 from typing import List, Optional
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 from dataclasses import dataclass
 
 class Settings(BaseSettings):
